6d7ed41d7601fa1038d2f4c6b6027e88b034c231
Momenti <seunghak@momenti.biz>
2026-05-11 17:40:44 +0900

    initialize jenkins-cicd

---
+++ src/main/java/com/exforum/jenkins/JenkinsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.exforum.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCicdApplication.class, args);
+    }
+
+}
